d13c1de646ab02554d0e2783df592a0a923927b6
Ruslan Askhadullin <ashadullinruslan@rambler.ru>
2023-04-28 22:42:07 +0200

    first try

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+        entityManager.flush();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        entityManager.createQuery("delete from User where id=: id")
+                .setParameter("id", id)
+                .executeUpdate();
+        entityManager.flush();
+    }
+
+    @Override
+    public void editUser(User user) {
+        entityManager.merge(user);
+        entityManager.flush();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User").getResultList();
+    }
+}
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserDao userDao;
+
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public void editUser(User user) {
+        userDao.editUser(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+}
